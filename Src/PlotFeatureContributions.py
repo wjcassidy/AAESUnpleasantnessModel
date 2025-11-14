cbbbde951c1648277074dd9f3ffe0554aded1c13
Will Cassidy <60139374+willcassidy00454@users.noreply.github.com>
2025-11-14 08:29:29 +0000

    Update feature contributions

--- Src/PlotFeatureContributions.py
+++ Src/PlotFeatureContributions.py
@@ -6,8 +6,8 @@ from matplotlib import rc
 if __name__ == "__main__":
     features = ["Colour.", "Flutter", "Asym.", "Curv.", "Damp."]
 
-    standardised_betas = [[0.127, 0.063, 0.175, 0.153, 0.136],
-                          [0.383, -0.065, -0.078, 0.134, -0.119]]
+    standardised_betas = [[0.170, 0.134, 0.143, 0.189, 0.115],
+                          [0.368, -0.103, -0.046, 0.107, -0.127]]
 
     # This is the "part" column in SPSS when showing "part and partial correlations"
     # Each column here is for each programme item
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     unique_proportions_of_data_variance = np.square(semi_partial_correlations)
     unique_proportions_of_model_variance = [unique_proportions_of_data_variance[i, :] / r_squares[i] for i in range(2)]
 
-    significances = [["***", "**", "***", "***", "***"],
-                     ["***", "**", "***", "***", "***"]]
+    significances = [["***", "***", "***", "***", "***"],
+                     ["***", "***", "", "***", "***"]]
 
     fig, axes = plt.subplots(2)
     fig.set_size_inches(4, 6)
@@ -33,7 +33,7 @@ if __name__ == "__main__":
         axes[plot_index].bar_label(p, labels=significances[plot_index], label_type='edge')
         axes[plot_index].set_title("b. Saxophone" if plot_index else "a. Handclaps")
         axes[plot_index].grid(zorder=0, axis="y")
-        axes[plot_index].set_ylim([-0.15, 0.45])
+        axes[plot_index].set_ylim([-0.17, 0.43])
         axes[plot_index].set_yticks([-0.1, 0.0, 0.1, 0.2, 0.3, 0.4])
         axes[plot_index].set_yticklabels(["-0.1", "0.0", "0.1", "0.2", "0.3", "0.4"])
         axes[plot_index].set_ylabel("Standardised Beta Coefficients")
